d8642e25c52d73592e6c7c8a8abf366547f4cb6c
Oppidan India <oppidanindia@gmail.com>
2026-06-15 14:04:53 +0530

    Fix eslint error

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FaArrowRight, FaPlay, FaIndustry, FaProjectDiagram, FaSmile, FaShieldAlt, FaQuoteLeft } from 'react-icons/fa';
 import CountUp from 'react-countup';
